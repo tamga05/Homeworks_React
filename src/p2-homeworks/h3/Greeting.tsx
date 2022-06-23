c35f918bbb5f876b53b33a859448800ab90077c0
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-06-24 00:33:58 +0300

    add Greeting.tsx style markup

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -30,12 +30,32 @@ const Greeting: React.FC<GreetingPropsType> = (
     //     border: '1px solid green'
     // };
 
+    const errorInput = s.errorInputClass;
+    const normalInput = s.inputInputClass;
+    const styleInput = error ? errorInput : normalInput; // styleInput
+
+    const errorButton = s.errorButton;
+    const normalButton = s.button;
+    const styleButton = error ? errorButton : normalButton; // styleButton
+
+    const errorSpan = s.errorTotalUsers;
+    const normalSpan = s.totalUsers;
+    const styleSpan = error ? errorSpan : normalSpan; // styleSpan
+
     return (
         <div>
             <div className={s.wrapperInput}>
-                <input value={name} onChange={setNameCallback} className={error ? s.errorInputClass : s.inputInputClass}/>
-                <button onClick={addUser} disabled={!name} className={error ? s.errorButton : s.button}>add</button>
-                <span className={error ? s.errorTotalUsers : s.totalUsers}>{totalUsers}</span>
+                {/*<input value={name} onChange={setNameCallback} className={error ? s.errorInputClass : s.inputInputClass}/>*/}
+                {/*<button onClick={addUser} disabled={!name} className={error ? s.errorButton : s.button}>add</button>*/}
+                {/*<span className={error ? s.errorTotalUsers : s.totalUsers}>{totalUsers}</span>*/}
+
+                {/*<input value={name} onChange={setNameCallback} className={error ? errorInput : normalInput}/>*/}
+                {/*<button onClick={addUser} disabled={!name} className={error ? errorButton : normalButton}>add</button>*/}
+                {/*<span className={error ? errorSpan : normalSpan}>{totalUsers}</span>*/}
+
+                <input value={name} onChange={setNameCallback} className={styleInput}/>
+                <button onClick={addUser} disabled={!name} title={'Enter at input your name and press add '} className={styleButton}>add</button>
+                <span className={styleSpan}>{totalUsers}</span>
             </div>
             <div className={s.wrapperErrorSpanClass}>
                 <span className={s.errorSpanClass}>{error}</span>
